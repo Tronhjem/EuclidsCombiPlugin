2500a341f829f204f0c024ea67627ba3cfe8f656
Christian Tronhjem <ctronhjem@gmail.com>
2025-05-22 14:29:29 +0200

    test for euclid

--- ORchestraUnitTests/Source/Test_EuclidAndRandom.h
+++ ORchestraUnitTests/Source/Test_EuclidAndRandom.h
@@ -10,5 +10,63 @@ public:
     
     void runTest() override
     {
+        {
+            beginTest ("Test Euclid sequence wrong defined");
+            
+            std::string file {"a = {2,4 \n test a"};
+            VM vm;
+            bool result = vm.Prepare(&file[0]);
+            
+            expect (result == false);
+        }
+        {
+            beginTest ("Test Euclid sequence wrong defined ");
+            
+            std::string file {"a = {2} \n test a"};
+            VM vm;
+            bool result = vm.Prepare(&file[0]);
+            
+            expect (result == false);
+        }
+        {
+            beginTest ("Test Euclid sequence");
+            
+            std::string file {"a = {2,4} \n test a"};
+            VM vm;
+            vm.Prepare(&file[0]);
+            
+            uChar result = vm.GetTopStackValue();
+            expect (result == 1);
+        }
+        {
+            beginTest ("Test Euclid sequence");
+            
+            std::string file {"a = {2,4} \n test a[1]"};
+            VM vm;
+            vm.Prepare(&file[0]);
+            
+            uChar result = vm.GetTopStackValue();
+            expect (result == 0);
+        }
+        {
+            beginTest ("Test Euclid sequence");
+            
+            std::string file {"a = {2,4} \n test a[2]"};
+            VM vm;
+            vm.Prepare(&file[0]);
+            
+            uChar result = vm.GetTopStackValue();
+            expect (result == 1);
+        }
+        {
+            beginTest ("Test Euclid sequence");
+            
+            std::string file {"a = {2,4} \n test a[3]"};
+            VM vm;
+            vm.Prepare(&file[0]);
+            
+            uChar result = vm.GetTopStackValue();
+            expect (result == 0);
+        }
     }
 };
