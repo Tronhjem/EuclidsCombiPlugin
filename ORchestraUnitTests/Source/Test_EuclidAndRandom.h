53abc1a38bd7e83bb28ebb75c15a41391f686696
Christian Tronhjem <ctronhjem@gmail.com>
2025-05-22 14:13:18 +0200

    add file for euclid test

---
+++ ORchestraUnitTests/Source/Test_EuclidAndRandom.h
@@ -0,0 +1,14 @@
+#pragma once
+
+using namespace juce;
+#include "../../Source/VM.h"
+
+class Test_EuclidAndRandom  : public UnitTest
+{
+public:
+    Test_EuclidAndRandom()  : UnitTest ("Test_OpCodeResults") {}
+    
+    void runTest() override
+    {
+    }
+};
