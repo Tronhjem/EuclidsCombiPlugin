b7df0d2fb96da09e334dc78547674773b5e734e8
Christian Tronhjem <ctronhjem@gmail.com>
2025-07-07 19:32:17 +0200

    fix clamp midi bug in scheduler

--- Source/MidiScheduler.cpp
+++ Source/MidiScheduler.cpp
@@ -13,8 +13,8 @@ void MidiScheduler::PostMidiNote(const uChar channel,
 {
     const int timeStampOff = timeStamp + durationInSamples;
     const int clampedChannel = std::clamp(static_cast<int>(channel), 0, 16);
-    const int clampedNote = std::clamp(static_cast<int>(noteNumber), 0, 16);
-    const unsigned char clampedVelocity = static_cast<unsigned char>(std::clamp(static_cast<int>(velocity), 0, 16));
+    const int clampedNote = std::clamp(static_cast<int>(noteNumber), 0, 127);
+    const unsigned char clampedVelocity = static_cast<unsigned char>(std::clamp(static_cast<int>(velocity), 0, 127));
 
     mScheduledMidiMessages.emplace_back(ScheduledMidi{juce::MidiMessage::noteOn(clampedChannel, clampedNote, clampedVelocity), timeStamp});
     mScheduledMidiMessages.emplace_back(ScheduledMidi{juce::MidiMessage::noteOff(clampedChannel, clampedNote), timeStampOff});
@@ -26,8 +26,8 @@ void MidiScheduler::PostMidiCC(const uChar channel,
                                const int timeStamp)
 {
     const int clampedChannel = std::clamp(static_cast<int>(channel), 0, 16);
-    const int clampedCC = std::clamp(static_cast<int>(cc), 0, 16);
-    const unsigned char clampedValue = static_cast<unsigned char>(std::clamp(static_cast<int>(value), 0, 16));
+    const int clampedCC = std::clamp(static_cast<int>(cc), 0, 127);
+    const unsigned char clampedValue = static_cast<unsigned char>(std::clamp(static_cast<int>(value), 0, 127));
 
     mScheduledMidiMessages.emplace_back(ScheduledMidi{juce::MidiMessage::controllerEvent(clampedChannel, clampedCC, clampedValue), timeStamp});
 }
