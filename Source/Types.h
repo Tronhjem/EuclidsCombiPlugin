fdc445d75564040f80b03485948544f786ee9aaa
Christian Tronhjem <ctronhjem@gmail.com>
2025-08-21 22:33:35 +0200

    cleanup typedef

--- Source/Types.h
+++ Source/Types.h
@@ -4,11 +4,6 @@
 #include "DataSequenceStep.h"
 
 typedef unsigned char uChar;
-//typedef uChar StepData;
 typedef DataSequenceStep StepData;
 
-
-//static_assert(sizeof(uChar) == 1);
-
-//#define EXPECTED_LOGIC_SEQUENCE_SIZE 32
-//static_assert(MAX_LOGIC_SEQUENCE_LENGTH == EXPECTED_LOGIC_SEQUENCE_SIZE);
+static_assert(sizeof(uChar) == 1);
